0523848509d161e5abb6baba96ead183a276b06f
queenyaa <ysgyan@gmail.com>
2024-04-16 17:58:42 -0700

    retracing

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -12,14 +12,14 @@ def canUnlockAll(boxes):
     Args:
         boxes (list of lists): contain keys used to unlock other lists
     """
-    keys = set([0])
-    opened = set()
+    nextbox = set()
 
-    while keys:
-        key = keys.pop()
-        opened.add(key)
-        for box in boxes[key]:
-            if box not in opened:
-                keys.add(box)
-
-    return len(opened) == len(boxes)
+    for id_box, box in enumerate(boxes):
+        if len(box) == 0 or id_box == 0:
+            nextbox.add(id_box)
+        for key in box:
+            if key < len(boxes) and key != id_box:
+                nextbox.add(key)
+        if len(nextbox) == len(boxes):
+            return True
+    return False
